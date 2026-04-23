754ed43c1f03ba28866c743ed18b9dd0a9ff6777
Ouna-the-Dataweaver <agafonovigor97@gmail.com>
2026-04-23 13:22:55 +0300

    chore: remove deprecated UI versions
    Drop V1CleanModern, V2GlassNeon, V3LightTabs and their stylesheets.
    Only V4GraphCentric remains active.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Original from "./versions/Original";
-import V1CleanModern from "./versions/V1CleanModern";
-import V2GlassNeon from "./versions/V2GlassNeon";
-import V3LightTabs from "./versions/V3LightTabs";
 import V4GraphCentric from "./versions/V4GraphCentric";
 
 function App() {
@@ -10,10 +6,6 @@ function App() {
     <BrowserRouter basename="/admin">
       <Routes>
         <Route path="/" element={<Navigate to="/v4" replace />} />
-        <Route path="/v0" element={<Original />} />
-        <Route path="/v1" element={<V1CleanModern />} />
-        <Route path="/v2" element={<V2GlassNeon />} />
-        <Route path="/v3" element={<V3LightTabs />} />
         <Route path="/v4" element={<V4GraphCentric />} />
       </Routes>
     </BrowserRouter>
